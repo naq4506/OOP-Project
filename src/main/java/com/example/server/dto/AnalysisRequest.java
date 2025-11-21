3525b557dbd8259bdfec65dc1a03d8c6258e13af
naq4506 <naq7838@gmail.com>
2025-11-21 20:14:28 +0700

    Update

---
+++ src/main/java/com/example/server/dto/AnalysisRequest.java
@@ -0,0 +1,45 @@
+package com.example.server.dto;
+
+public class AnalysisRequest {
+
+    private String disasterName;
+    private String startDate;
+    private String endDate;
+    private String keyword;
+    private String analysisType;
+
+    public String getDisasterName() {
+        return disasterName; 
+    }
+    public void setDisasterName(String disasterName) {
+        this.disasterName = disasterName; 
+    }
+
+    public String getStartDate() {
+        return startDate; 
+    }
+    public void setStartDate(String startDate) {
+        this.startDate = startDate; 
+    }
+
+    public String getEndDate() {
+        return endDate; 
+    }
+    public void setEndDate(String endDate) { 
+        this.endDate = endDate; 
+    }
+
+    public String getKeyword() { 
+        return keyword; 
+    }
+    public void setKeyword(String keyword) { 
+        this.keyword = keyword; 
+    }
+
+    public String getAnalysisType() { 
+        return analysisType; 
+    }
+    public void setAnalysisType(String analysisType) { 
+        this.analysisType = analysisType; 
+    }
+}
